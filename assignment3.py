410cac2bf16bae175873ce664f38812047160af5
cuban.quant <cuban.quant@gmail.com>
2020-10-25 22:28:51 -0400

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,15 @@
+import argparse
+# other imports go here
+
+
+def main(url):
+    print(f"Running main with URL = {url}...")
+
+
+if __name__ == "__main__":
+    """Main entry point"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
+    args = parser.parse_args()
+    main(args.url)
+    
